a4a1c876172925c8841dc6400a274c0e48029fcf
Agnaou Zineb <agnaou.zineb@gmail.com>
2021-10-04 14:37:35 +0200

    -

--- EDA.R
+++ EDA.R
@@ -55,46 +55,119 @@ df
 #df_general<-df %>% filter(`Age group` == "All Ages" & State == "United States" & Group == "By Total" ) 
 
 
+##############################################################################################################
+# plot the data using ggplot################################################################################
 
-# plot the data using ggplot
+#  function to plot: pick the data set
+LevelPlots <- function(dataframe, variable, name_variable, Chosen_Factor) {
+  
+  ggplot(data = dataframe, aes(x = `Start Date` , y =  variable)) +
+    geom_point(aes(colour = factor(Chosen_Factor)))+
+    labs(x = "Start Date",
+         y = name_variable )
+ 
+
+}
+
 #require(gridExtra)
 
-ggplot(data = df, aes(x = `Start Date` , y =  `Total Deaths`)) +
-  geom_point(aes(colour = factor(`Group`)))+
-  labs(x = "Start Date",
-       y = "Total deaths")
-
-ggplot(data = df, aes(x = `Start Date` , y =  `Total Deaths`)) +
-  geom_point(aes(colour = factor(`Place of Death`)))+
-  labs(x = "Start Date",
-       y = "Total deaths")
-
-ggplot(data = df, aes(x = `Start Date` , y =  `Total Deaths`)) +
-  geom_point(aes(colour = factor(`HHS Region`)))+
-  labs(x = "Start Date",
-       y = "Total deaths")
-
-ggplot(data = df, aes(x = `Start Date` , y =  `Total Deaths`)) +
-  geom_point(aes(colour = factor(`State`)))+
-  labs(x = "Start Date",
-       y = "Total deaths")
-
-ggplot(data = df, aes(x = `Start Date` , y =  `Total Deaths`)) +
-  geom_point(aes(colour = factor(`Age group`)))+
-  labs(x = "Start Date",
-       y = "Total deaths")
 
 
-# p <- ggplot(data = df, aes(x = `Start Date` , y =  `Total Deaths`, shape = factor(`Age group`)))
-# p +
-#   geom_point(aes(colour = factor(`Age group`)), size = 4) +
-#   geom_point(colour = "grey90", size = 1.5)
+#######################################################################################################
+# Total deaths : 
+LevelPlots(df, df$`Total Deaths`,"Total Deaths",df$Group)
+LevelPlots(df, df$`Total Deaths`,"Total Deaths",df$`HHS Region`)
+LevelPlots(df, df$`Total Deaths`,"Total Deaths",df$State)
+LevelPlots(df, df$`Total Deaths`,"Total Deaths",df$`Place of Death`)
+LevelPlots(df, df$`Total Deaths`,"Total Deaths",df$`Age group`)
 
-# d <- ggplot(data = df, aes(x = `Start Date` , y =  `Total Deaths`,shape = factor(`Age group`)))
-# d + geom_point(alpha = 1/10)
-# 
-# 
-# ggplot(data = df, aes(x = `Start Date` , y =  `Total Deaths`)) +
-#   geom_point(aes(colour = factor(`Age group`)), alpha = 1/10)+
-#   labs(x = "Date",
-#        y = "Total deaths")
+
+# "Pneumonia Deaths"  
+LevelPlots(df, df$`Pneumonia Deaths`,"Pneumonia Deaths",df$Group)
+LevelPlots(df, df$`Pneumonia Deaths`,"Pneumonia Deaths",df$`HHS Region`)
+LevelPlots(df, df$`Pneumonia Deaths`,"Pneumonia Deaths",df$State)
+LevelPlots(df, df$`Pneumonia Deaths`,"Pneumonia Deaths",df$`Place of Death`)
+LevelPlots(df, df$`Pneumonia Deaths`,"Pneumonia Deaths",df$`Age group`)
+
+
+# "Pneumonia and COVID-19 Deaths" 
+
+LevelPlots(df, df$`Pneumonia and COVID-19 Deaths`,"Pneumonia and COVID-19 Deaths",df$Group)
+LevelPlots(df, df$`Pneumonia and COVID-19 Deaths`,"Pneumonia and COVID-19 Deaths",df$`HHS Region`)
+LevelPlots(df, df$`Pneumonia and COVID-19 Deaths`,"Pneumonia and COVID-19 Deaths",df$State)
+LevelPlots(df, df$`Pneumonia and COVID-19 Deaths`,"Pneumonia and COVID-19 Deaths",df$`Place of Death`)
+LevelPlots(df, df$`Pneumonia and COVID-19 Deaths`,"Pneumonia and COVID-19 Deaths",df$`Age group`)
+
+# "Influenza Deaths"    
+LevelPlots(df, df$`Influenza Deaths`,"Influenza Deaths",df$Group)
+LevelPlots(df, df$`Influenza Deaths`,"Influenza Deaths",df$`HHS Region`)
+LevelPlots(df, df$`Influenza Deaths`,"Influenza Deaths",df$State)
+LevelPlots(df, df$`Influenza Deaths`,"Influenza Deaths",df$`Place of Death`)
+LevelPlots(df, df$`Influenza Deaths`,"Influenza Deaths",df$`Age group`)
+
+# "Pneumonia, Influenza, or COVID-19 Deaths" 
+
+LevelPlots(df, df$`Pneumonia, Influenza, or COVID-19 Deathss`,"Pneumonia, Influenza, or COVID-19 Deathss",df$Group)
+LevelPlots(df, df$`Pneumonia, Influenza, or COVID-19 Deathss`,"Pneumonia, Influenza, or COVID-19 Deathss",df$`HHS Region`)
+LevelPlots(df, df$`Pneumonia, Influenza, or COVID-19 Deathss`,"Pneumonia, Influenza, or COVID-19 Deathss",df$State)
+LevelPlots(df, df$`Pneumonia, Influenza, or COVID-19 Deathss`,"Pneumonia, Influenza, or COVID-19 Deathss",df$`Place of Death`)
+LevelPlots(df, df$`Pneumonia, Influenza, or COVID-19 Deathss`,"Pneumonia, Influenza, or COVID-19 Deathss",df$`Age group`)
+
+
+####################################################################################################
+# trying to understand the start/end date part:
+unique(df$`End Date`)
+unique(df$`Start Date`)
+unique(df$`Age group`) # maybe delete the firt level
+unique(df$Group)
+unique(df$`HHS Region`)
+unique(df$State) # maybe delete the firt level
+unique(df$`Place of Death`)# maybe delete the firt level
+
+
+df_specific<- df %>% filter(`Age group` != "All Ages" & State != "United States" & `Place of Death` != "Total - All Places of Death")
+df_specific
+
+
+###
+# Total deaths : 
+LevelPlots(df_specific, df_specific$`Total Deaths`,"Total Deaths",df_specific$Group)
+LevelPlots(df_specific, df_specific$`Total Deaths`,"Total Deaths",df_specific$`HHS Region`)
+LevelPlots(df_specific, df_specific$`Total Deaths`,"Total Deaths",df_specific$State)
+LevelPlots(df_specific, df_specific$`Total Deaths`,"Total Deaths",df_specific$`Place of Death`)
+LevelPlots(df_specific, df_specific$`Total Deaths`,"Total Deaths",df_specific$`Age group`)
+
+
+# "Pneumonia Deaths"  
+LevelPlots(df_specific, df_specific$`Pneumonia Deaths`,"Pneumonia Deaths",df_specific$Group)
+LevelPlots(df_specific, df_specific$`Pneumonia Deaths`,"Pneumonia Deaths",df_specific$`HHS Region`)
+LevelPlots(df_specific, df_specific$`Pneumonia Deaths`,"Pneumonia Deaths",df_specific$State)
+LevelPlots(df_specific, df_specific$`Pneumonia Deaths`,"Pneumonia Deaths",df_specific$`Place of Death`)
+LevelPlots(df_specific, df_specific$`Pneumonia Deaths`,"Pneumonia Deaths",df_specific$`Age group`)
+
+
+# "Pneumonia and COVID-19 Deaths" 
+
+LevelPlots(df_specific, df_specific$`Pneumonia and COVID-19 Deaths`,"Pneumonia and COVID-19 Deaths",df_specific$Group)
+LevelPlots(df_specific, df_specific$`Pneumonia and COVID-19 Deaths`,"Pneumonia and COVID-19 Deaths",df_specific$`HHS Region`)
+LevelPlots(df_specific, df_specific$`Pneumonia and COVID-19 Deaths`,"Pneumonia and COVID-19 Deaths",df_specific$State)
+LevelPlots(df_specific, df_specific$`Pneumonia and COVID-19 Deaths`,"Pneumonia and COVID-19 Deaths",df_specific$`Place of Death`)
+LevelPlots(df_specific, df_specific$`Pneumonia and COVID-19 Deaths`,"Pneumonia and COVID-19 Deaths",df_specific$`Age group`)
+
+# "Influenza Deaths"    
+LevelPlots(df_specific, df_specific$`Influenza Deaths`,"Influenza Deaths",df_specific$Group)
+LevelPlots(df_specific, df_specific$`Influenza Deaths`,"Influenza Deaths",df_specific$`HHS Region`)
+LevelPlots(df_specific, df_specific$`Influenza Deaths`,"Influenza Deaths",df_specific$State)
+LevelPlots(df_specific, df_specific$`Influenza Deaths`,"Influenza Deaths",df_specific$`Place of Death`)
+LevelPlots(df_specific, df_specific$`Influenza Deaths`,"Influenza Deaths",df_specific$`Age group`)
+
+# "Pneumonia, Influenza, or COVID-19 Deaths" 
+
+LevelPlots(df_specific, df_specific$`Pneumonia, Influenza, or COVID-19 Deathss`,"Pneumonia, Influenza, or COVID-19 Deathss",df_specific$Group)
+LevelPlots(df_specific, df_specific$`Pneumonia, Influenza, or COVID-19 Deathss`,"Pneumonia, Influenza, or COVID-19 Deathss",df_specific$`HHS Region`)
+LevelPlots(df_specific, df_specific$`Pneumonia, Influenza, or COVID-19 Deathss`,"Pneumonia, Influenza, or COVID-19 Deathss",df_specific$State)
+LevelPlots(df_specific, df_specific$`Pneumonia, Influenza, or COVID-19 Deathss`,"Pneumonia, Influenza, or COVID-19 Deathss",df_specific$`Place of Death`)
+LevelPlots(df_specific, df_specific$`Pneumonia, Influenza, or COVID-19 Deathss`,"Pneumonia, Influenza, or COVID-19 Deathss",df_specific$`Age group`)
+
+
+
